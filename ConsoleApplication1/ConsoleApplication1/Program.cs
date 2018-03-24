0660612d42c359bef1963d709158a83860a3fcf5
tnayanam <nayatanuj@gmail.com>
2018-03-24 17:49:28 -0400

    Dll,exe adding reference,class library

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -37,4 +37,10 @@ namespace ProjectB
         }
     }
 }
-// Namespace helps us to manage our code also they help us in avoiding name conflict (e.g: Display());
+// Namespace helps us to manage our code also they help us in avoiding name conflict (e.g: Display());'
+
+// If you create a new project then you need to create is a class library. Because it will not be used standalone it will
+// be more like a helper, also it will be built as a DLL NOT an EXE
+// where as main project will have an EXE. 
+// Also just creating a project does not help, one also needs to add reference of those dll's in the amin project. so clikc on main project and add references for those dll's
+// also using statement will be required to actually use functionality of oter project in main project.
