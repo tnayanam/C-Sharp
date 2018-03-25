3310d22a77442ce11e65f600d50ca39d783a6ce5
tnayanam <nayatanuj@gmail.com>
2018-03-25 15:56:50 -0400

    Encapsulation achieved using getter and setter

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     // Business Rule
     /*
@@ -8,18 +10,50 @@
      */
     class Test
     {
-        public int Passmarks;
-        public string Name;
-        public int Id;
+        private int _Passmarks;
+        private string _Name;
+        private int _Id;
+
+        public void SetID(int Id)
+        {
+            if (Id < 0)
+                throw new Exception("Id cannot be negative");
+            _Id = Id;
+        }
+        public int GetID()
+        {
+            return _Id;
+        }
+
+        public void SetName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+                throw new Exception("No Name Given");
+            _Name = Name;
+        }
+
+        public string GetName()
+        {
+            return string.IsNullOrEmpty(_Name) ? "No Name" : _Name;
+        }
+
+        public int GetMarks()
+        {
+            return _Passmarks;
+        }
+
+
     }
     class Program
     {
         static void Main(string[] args)
         {
             Test t1 = new Test();
-            t1.Id = -1;
-            t1.Name = null;
-            t1.Passmarks = 0;
+            //t1.passingMarks - passing marks cannot be set now.
+            //t1.SetID(-2); // this will throw exception
+            Console.WriteLine(t1.GetName()); // No Name output
+            t1.SetName(null); // Exception
+
         }
     }
 }
