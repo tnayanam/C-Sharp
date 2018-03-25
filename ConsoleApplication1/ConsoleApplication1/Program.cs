e19f0548b8437117c885b77a330cb9edabcbb8dd
tnayanam <nayatanuj@gmail.com>
2018-03-24 20:32:45 -0400

    Basic Class Example

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,46 +1,30 @@
-﻿using PATA = ProjectA;
-using PATB = ProjectB;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
+    // Simple Class Example
     class Program
     {
         static void Main(string[] args)
         {
-            ProjectA.ClassA.Display(); // one way of calling
-            ProjectB.ClassA.Display();
-            PATA.ClassA.Display(); // Another way
-            PATB.ClassA.Display();
+            Customer c1 = new Customer("Tanuj", "Nayanam");
+            c1.Display();
         }
     }
-}
 
-namespace ProjectA
-{
-    class ClassA
+    class Customer
     {
-        public static void Display()
+        private string _lastName; // keep underscore for private varibles name
+        private string _firstName;
+
+        public Customer(string FirstName, string LastName)
         {
-            System.Console.WriteLine("I am from ProjectA.ClassA");
+            _firstName = FirstName;
+            _lastName = LastName;
+        }
+
+        public void Display()
+        {
+            System.Console.WriteLine("FirstName is {0} and LastName is {1}", _firstName, _lastName);
         }
     }
 }
 
-
-namespace ProjectB
-{
-    class ClassA
-    {
-        public static void Display()
-        {
-            System.Console.WriteLine("I am from ProjectB.ClassA");
-        }
-    }
-}
-// Namespace helps us to manage our code also they help us in avoiding name conflict (e.g: Display());'
-
-// If you create a new project then you need to create is a class library. Because it will not be used standalone it will
-// be more like a helper, also it will be built as a DLL NOT an EXE
-// where as main project will have an EXE. 
-// Also just creating a project does not help, one also needs to add reference of those dll's in the amin project. so clikc on main project and add references for those dll's
-// also using statement will be required to actually use functionality of oter project in main project.
