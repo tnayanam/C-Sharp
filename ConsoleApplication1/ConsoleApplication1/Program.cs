17e18ac359c1b55fa83e67ed1d4dc32d73d383a3
tnayanam <nayatanuj@gmail.com>
2018-03-28 07:18:30 -0400

    Problem without delegates

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,22 +1,40 @@
-﻿namespace ConsoleApplication1
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication1
 {
-    public delegate void DelForMethod(string Message);
-    // Delegate
+    // Problem when delegates are not present!
+
+    class Employee
+    {
+        public int Salary;
+        public string Name;
+        public static void PromoteEmployee(List<Employee> empL)
+        {
+            foreach (var emp in empL)
+            {
+                if (emp.Salary > 4000)
+                    System.Console.WriteLine(emp.Name + " promoted");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            DelForMethod d1 = new DelForMethod(Add);
-            d1("Hmmm");
-        }
-        public static void Add(string strMessage)
-        {
-            System.Console.WriteLine(strMessage);
+            List<Employee> empL = new List<Employee>
+            {
+                new Employee {Name = "Patrick", Salary = 4500 },
+                new Employee {Name = "Messy", Salary = 500 },
+                new Employee {Name = "Gordon", Salary = 200 },
+                new Employee {Name = "Marey", Salary = 5500 }
+            };
+            Employee.PromoteEmployee(empL);
         }
     }
 
-    // delegate is a type safe function pointer
-    // signature of delegate must match the signature of the method it points too.
+    // now if someone wants to change the logic of promotion of employee it has to come here and change the class methods. that sthe problem.
+    // delegates are extensively used in framweork development as we can inject our own logic to it w/o modifying the framework code.
 }
 
 
