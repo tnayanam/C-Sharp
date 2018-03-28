f67c3affb29ef2c43cd7e89e87119dd07ae1cbd6
tnayanam <nayatanuj@gmail.com>
2018-03-28 05:00:45 -0400

    Problem with multiple inheritance

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,43 +1,42 @@
 ﻿namespace ConsoleApplication1
 {
-    abstract class Customer
+    // problem with multiple inheritance
+    class A
     {
-        public abstract int Print(); // it cannot have the implementaton
-        public void Tan()
+        public virtual void Print()
         {
 
         }
     }
 
-    // an abstract class is incomplete so cannot be instantiated.
-    // an abstract class cannot be selaed because an abstract class is meant to over public
-    // AN ABSTRACT CLASS MAy or may not contain abstract members
-    // a non abstract class deriving from abstract class must implement al the methods of abstract class.
-    // an abstract class cannot be sealed
-    // we cannot create instance of wither interface oir abstract cloass.
-    class Program : Customer
+    class B : A
+    {
+        public override void Print()
+        {
+            //base.Print();
+        }
+    }
+
+    class C : A
+    {
+        public override void Print()
+        {
+            //base.Print();
+        }
+    }
+
+    class D : C, B // compiler merror but suppose if this was allowed and we create a instance of C and call the "POrint method" then which implementation of print method
+                   //should be calledf. B's implementation or C's implementation// this is called DIAMOND PROBLEM
+    {
+
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Program p1 = new Program();
-
-
-        }
-
-        public override int Print()
-        {
-            System.Console.WriteLine("I am in non abstract class");
-            return 0;
         }
     }
 }
 
-/*
- * difference between Abstract and Interface
- * we can have a method implementation in abstract but interface cant have method implementation.
- * interfaces cant haqve access modifiers as they are public by default'
- * and class members are preivate by default
- * interface cannot cobntain fields but absatract class can have fields
- * abstract class members can have access modifiers but interface cannot.
- * 
- */
+
