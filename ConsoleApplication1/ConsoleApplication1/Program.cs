1d76a1eee484ebab4ccff8cbb28f16bc819e6ecf
tnayanam <nayatanuj@gmail.com>
2018-03-29 22:41:37 -0400

    Custom Exception

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,63 +4,42 @@
  */
 
 using System;
-using System.IO;
-
+using System.Runtime.Serialization;
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try // this exception is added , because what if the the exception occurs in the first catch block in that case this try will help.
+            try
             {
-                try
-                {
-                    Console.WriteLine("Enter First Number");
-                    int FN = Convert.ToInt32(Console.ReadLine());
-
-
-                    Console.WriteLine("Enter Second Number");
-                    int SN = Convert.ToInt32(Console.ReadLine());
-
-
-                    int Result = FN / SN;
-                    Console.WriteLine("Result = {0}", Result);
-                }
-                catch (Exception ex)
-                {
-                    string filePath = @"C:\Sample Files\Log.txt";
-                    if (File.Exists(filePath))
-                    {
-                        StreamWriter sw = new StreamWriter(filePath);
-                        sw.Write(ex.GetType().Name + ex.Message + ex.StackTrace);
-                        sw.Close();
-                        Console.WriteLine("There is a problem! Plese try later");
-                    }
-                    else
-                    {
-                        //To retain the original exception pass it as a parameter
-                        //to the constructor, of the current exception
-                        throw new FileNotFoundException(filePath + " Does not Exist", ex);
-                    }
-                }
+                throw new UserAlreadyLoggedInException("User Already logged in");
             }
-            catch (Exception ex)
+            catch (UserAlreadyLoggedInException ex)
             {
-                //ex.Message will give the current exception message
-                Console.WriteLine("Current or Outer Exception = " + ex.Message);
-
-
-                //Check if inner exception is not null before accessing Message property
-                //else, you may get Null Reference Excception
-                if (ex.InnerException != null)
-                {
-                    Console.WriteLine("Inner Exception = ", ex.InnerException.Message);
-                }
+                Console.WriteLine(ex.Message);
             }
         }
     }
 
+
+    [Serializable]
+    public class UserAlreadyLoggedInException : Exception
+    {
+        public UserAlreadyLoggedInException(string message)
+            : base(message)
+        {
+        }
+
+
+        public UserAlreadyLoggedInException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
 }
-
-
