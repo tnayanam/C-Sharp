522423bc76b66aa9ead756fed883db81ba3dd38f
tnayanam <nayatanuj@gmail.com>
2018-03-29 22:26:11 -0400

    InnerException

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,34 +3,64 @@
  * it is a delegate the references more than one function, when we invoke a multicast delegate all the delegate it points to gets invoked
  */
 
+using System;
+using System.IO;
+
 namespace ConsoleApplication1
 {
-    delegate void Del();
     class Program
     {
         static void Main(string[] args)
         {
-            Del d1 = new Del(HelloOne);
-            Del d2 = new Del(HelloTwo);
-            Del d3 = d1 + d2;
-            d3();
-            /*
-             * Output:
-             * One
-             * Two
-             */
+            try // this exception is added , because what if the the exception occurs in the first catch block in that case this try will help.
+            {
+                try
+                {
+                    Console.WriteLine("Enter First Number");
+                    int FN = Convert.ToInt32(Console.ReadLine());
 
-        }
-        public static void HelloOne()
-        {
-            System.Console.WriteLine("One");
-        }
 
-        public static void HelloTwo()
-        {
-            System.Console.WriteLine("Two");
+                    Console.WriteLine("Enter Second Number");
+                    int SN = Convert.ToInt32(Console.ReadLine());
+
+
+                    int Result = FN / SN;
+                    Console.WriteLine("Result = {0}", Result);
+                }
+                catch (Exception ex)
+                {
+                    string filePath = @"C:\Sample Files\Log.txt";
+                    if (File.Exists(filePath))
+                    {
+                        StreamWriter sw = new StreamWriter(filePath);
+                        sw.Write(ex.GetType().Name + ex.Message + ex.StackTrace);
+                        sw.Close();
+                        Console.WriteLine("There is a problem! Plese try later");
+                    }
+                    else
+                    {
+                        //To retain the original exception pass it as a parameter
+                        //to the constructor, of the current exception
+                        throw new FileNotFoundException(filePath + " Does not Exist", ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //ex.Message will give the current exception message
+                Console.WriteLine("Current or Outer Exception = " + ex.Message);
+
+
+                //Check if inner exception is not null before accessing Message property
+                //else, you may get Null Reference Excception
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception = ", ex.InnerException.Message);
+                }
+            }
         }
     }
+
 }
 
 
