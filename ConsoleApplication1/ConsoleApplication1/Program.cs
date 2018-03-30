74125e68ba9ad3a9fad28585b16411af8017f337
tnayanam <nayatanuj@gmail.com>
2018-03-29 22:50:39 -0400

    Exception Abuse using exception for handling logic of code

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,35 +11,42 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            try
-            {
-                throw new UserAlreadyLoggedInException("User Already logged in");
-            }
-            catch (UserAlreadyLoggedInException ex)
-            {
-                Console.WriteLine(ex.Message);
+                try
+                {
+                    Console.WriteLine("Please enter Numerator");
+                    int Numerator = Convert.ToInt32(Console.ReadLine());
+
+
+                    Console.WriteLine("Please enter Denominator");
+                    //Convert.ToInt32() can throw FormatException, if the entered value
+                    //cannot be converted to integer. So use int.TryParse() instead
+                    int Denominator = Convert.ToInt32(Console.ReadLine());
+
+
+                    int Result = Numerator / Denominator;
+
+
+                    Console.WriteLine("Result = {0}", Result);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Only numbers are allowed!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Only numbers between {0} & {1} are allowed",
+                        Int32.MinValue, Int32.MaxValue);
+
+
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Denominator cannot be zero");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
-
-
-    [Serializable]
-    public class UserAlreadyLoggedInException : Exception
-    {
-        public UserAlreadyLoggedInException(string message)
-            : base(message)
-        {
-        }
-
-
-        public UserAlreadyLoggedInException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-
-        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-    }
-}
