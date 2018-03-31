794e87d8dfb8de1b04fb63fc927a81c1f6f1fdc1
tnayanam <nayatanuj@gmail.com>
2018-03-31 17:52:17 -0400

    Attribute Obsolete

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,57 +1,42 @@
-﻿// IMPORTANT EXAMPLE 1 ENUM
+﻿
+using System;
+using System.Collections.Generic;
 
-//using System;
-//public class Enums
-//{
-//    public static void Main()
-//    {
-//        int[] Values = (int[])Enum.GetValues(typeof(Gender));
-//        Console.WriteLine("Gender Enum Values");
-//        foreach (int value in Values)
-//        {
-//            Console.WriteLine(value);
-//        }
-
-//        Console.WriteLine();
-//        string[] Names = Enum.GetNames(typeof(Gender));
-//        Console.WriteLine("Gender Enum Names");
-//        foreach (string Name in Names)
-//        {
-//            Console.WriteLine(Name);
-//        }
-//    }
-//}
-//public enum Gender : int
-//{
-//    Unknown = 1,
-//    Male = 2,
-//    Female = 3
-//}
-
-// IMPORTANT EXAMPLE 2 ENUM
-
-/*
- * Class, structs, enums, interfcaes, delegates are the TYPES
- * Field, propertioes, constructors, methods are the TYPE MEMBERS
- * TYPES can only have INTERNAL AND PUBLIC access modifier
- * TYPES MEMBERS can have Provate, protected, inrtenal, propetted internal, public
- */
-
-public class Enums
+public class Mains
 {
     public static void Main()
     {
+        Calculator.Add(3, 4); //old clients using this one.
+    }
+}
 
+public class Calculator
+{
+    // old method
+    [Obsolete("Please use a method int Add(List<int> Numbers)")] // after adding this green squiggly and warning will home. 
+    public static int Add(int FN, int SN)
+    {
+        return FN + SN;
+    }
+
+    public static int Add(List<int> Numbers)
+    {
+        int Sum = 0;
+        foreach (var num in Numbers)
+        {
+            Sum += num;
+        }
+        return Sum;
     }
 }
 
 /*
- * public : available everywhere
- * private: only inside the class
- * protected: accessible in the derived class.
- * internal:  anywhere within the containing assembly
- * protected internal:  anywhere within the containing assmebly and from anywhere from derived class in any other assembly.
+ * Attributes allow us to add declarative information to your programs. this information can be then queried at runtime using reflection
+ * Example of atribute [Obsolete], [Serializable]
  * 
- * default access modifiers of type  members is private
- * default access modfiers of type is internal
+ * For this example suppose we have  a  clauclator  class and we have a siomple method to  add two number, and then someone asked for a method of  adding 3  numbers
+ and then someone  asked for method  of  adding 4 numbers. Now we cannot keep on adding thses methods in our caluclator class so we decided to crate an  add method
+ which can take a list of intetger and SUM them. But th problem is our new calculator clients wont know about this function. Also we cannot remvoe the olde Add methods 
+ because it will break all the cleints who have coded for this method.  So we need to communicate the cients that we  have a improvedf add method without breaking theri coide.
+ so as soon as some one hovers they will  see that new method is there now. Also in compile time it will throw  warning so that they get notified and alrmedf
  */
