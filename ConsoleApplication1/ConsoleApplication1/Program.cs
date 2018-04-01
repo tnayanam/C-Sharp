f4899f99a0fefa10917430e91effafd89f06f0e9
tnayanam <nayatanuj@gmail.com>
2018-04-01 16:51:59 -0400

    Receive value from delegate

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿
+using System;
 using System.Threading;
 
 namespace ThreadStartDelegateExample
 {
+    // Step 1: Create a callback delegate. The actual callback method 
+    // signature should match with the signature of this delegate.
+    public delegate void SumOfNumbersCallback(int sumOfNumbers);
+
+    // Step 2: Create Number class to compute the sum of numbers and 
+    // to call the callback method
+    class Number
+    {
+        // The traget number this class needs to compute the sum of numbers
+        int _target;
+
+        // Delegate to call when the the Thread function completes computing the sum of numbers 
+        SumOfNumbersCallback _callbackMethod;
+
+        // Constructor to initialize the target number and the callback delegateinitialize
+        public Number(int target, SumOfNumbersCallback callbackMethod)
+        {
+            this._target = target;
+            this._callbackMethod = callbackMethod;
+        }
+
+        // This thread function computes the sum of numbers and then invokes
+        // the callback method passing it the sum of numbers
+        public void ComputeSumOfNumbers()
+        {
+            int sum = 0;
+            for (int i = 1; i <= _target; i++)
+            {
+                sum = sum + i;
+            }
+
+            if (_callbackMethod != null)
+            {
+                _callbackMethod(sum);
+            }
+        }
+    }
+
+    // Step 3: This class consumes the Number class created in Step 2
     class Program
     {
+        // Callback method that will receive the sum of numbers
+        public static void PrintSumOfNumbers(int sum)
+        {
+            Console.WriteLine("Sum of numbers is " + sum);
+        }
+
         public static void Main()
         {
             // Prompt the user for the target number
@@ -12,39 +58,19 @@ namespace ThreadStartDelegateExample
             // Read from the console and store it in target variable
             int target = Convert.ToInt32(Console.ReadLine());
 
-            // Create an instance of the Number class, passing it
-            // the target number that was read from the console
-            Number number = new Number(target);
-            // Specify the Thread function
-            Thread T1 = new Thread(new ThreadStart(number.PrintNumbers));
-            // Alternatively we can just use Thread class constructor as shown below
-            // Thread T1 = new Thread(number.PrintNumbers);
+            // Create an instance of callback delegate and to it's constructor
+            // pass the name of the callback function (PrintSumOfNumbers)
+            SumOfNumbersCallback callbackMethod = new SumOfNumbersCallback(PrintSumOfNumbers);
+
+            // Create an instance of Number class and to it's constrcutor pass the target
+            // number and the instance of callback delegate
+            Number number = new Number(target, callbackMethod);
+
+            // Create an instance of Thread class and specify the Thread function to invoke
+            Thread T1 = new Thread(new ThreadStart(number.ComputeSumOfNumbers));
             T1.Start();
-        }
-    }
-
-    // Number class also contains the data it needs to print the numbers
-    class Number
-    {
-        int _target;
-        // When an instance is created, the target number needs to be specified
-        public Number(int target)
-        {
-            // The targer number is then stored in the class private variable _target
-            this._target = target;
-        }
-
-        // Function prints the numbers from 1 to the traget number that the user provided
-        public void PrintNumbers()
-        {
-            for (int i = 1; i <= _target; i++)
-            {
-                Console.WriteLine(i);
-            }
         }
     }
 }
 
-// see how cleverly during the class instance we need to provide the target by this now we will use threadstart delegate asd
-// we no more need to pass the data via delegate call. Also, note that we have not provided default constructor int he Number
-// class so one has to pass the target number. thus how cleverly we made everything type sae.
+// here we will receiive value from callback delegate. NICE and COMPLEX
