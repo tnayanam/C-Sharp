22e51151b5198317c3a8f593281875556f1596ae
tnayanam <nayatanuj@gmail.com>
2018-03-31 21:20:59 -0400

    Reflection

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,42 +1,86 @@
-﻿
-using System;
-using System.Collections.Generic;
-
-public class Mains
+﻿using System;
+using System.Reflection;
+namespace Pragim
 {
-    public static void Main()
+    public class MainClass
     {
-        Calculator.Add(3, 4); //old clients using this one.
-    }
-}
-
-public class Calculator
-{
-    // old method
-    [Obsolete("Please use a method int Add(List<int> Numbers)")] // after adding this green squiggly and warning will home. 
-    public static int Add(int FN, int SN)
-    {
-        return FN + SN;
-    }
-
-    public static int Add(List<int> Numbers)
-    {
-        int Sum = 0;
-        foreach (var num in Numbers)
+        private static void Main()
         {
-            Sum += num;
+            // Get the Type Using GetType() static method
+            Type T = Type.GetType("Pragim.Customer");
+
+            Customer c2 = new Customer();
+            Type T1 = c2.GetType(); //get type when you have just the object 
+            // Print the Type details
+            Console.WriteLine("Full Name = {0}", T.FullName);
+            Console.WriteLine("Just the Class Name = {0}", T.Name);
+            Console.WriteLine("Just the Namespace = {0}", T.Namespace);
+            Console.WriteLine();
+            // Print the list of Methods
+            Console.WriteLine("Methods in Customer Class");
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                // Print the Return type and the name of the method
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+            Console.WriteLine();
+            //  Print the Properties
+            Console.WriteLine("Properties in Customer Class");
+            PropertyInfo[] properties = T.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                // Print the property type and the name of the property
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+            Console.WriteLine();
+            //  Print the Constructors
+            Console.WriteLine("Constructors in Customer Class");
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
         }
-        return Sum;
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public Customer(int ID, string Name)
+        {
+            this.Id = ID;
+            this.Name = Name;
+        }
+
+
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+
+
+        public void PrintID()
+        {
+            Console.WriteLine("ID = {0}", this.Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name = {0}", this.Name);
+        }
     }
 }
+
 
 /*
- * Attributes allow us to add declarative information to your programs. this information can be then queried at runtime using reflection
- * Example of atribute [Obsolete], [Serializable]
+ * Reflection is the ability of inspecting an assembly metadata at runtime. It is used to find all 
+ * types in an assembly and/or dynamically invoke methods in an assembly
  * 
- * For this example suppose we have  a  clauclator  class and we have a siomple method to  add two number, and then someone asked for a method of  adding 3  numbers
- and then someone  asked for method  of  adding 4 numbers. Now we cannot keep on adding thses methods in our caluclator class so we decided to crate an  add method
- which can take a list of intetger and SUM them. But th problem is our new calculator clients wont know about this function. Also we cannot remvoe the olde Add methods 
- because it will break all the cleints who have coded for this method.  So we need to communicate the cients that we  have a improvedf add method without breaking theri coide.
- so as soon as some one hovers they will  see that new method is there now. Also in compile time it will throw  warning so that they get notified and alrmedf
+ * Use ofd reflection
+ * late binding
+ * 
  */
+
