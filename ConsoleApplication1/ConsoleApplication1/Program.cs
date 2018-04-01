e65b819945af4c656497cfc3c75ec6ad24112c73
tnayanam <nayatanuj@gmail.com>
2018-04-01 09:50:53 -0400

    string builder and string

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 
 namespace Pragim
 {
@@ -6,18 +6,28 @@ namespace Pragim
     {
         private static void Main()
         {
-            Customer c1 = new Customer();
-            System.Console.WriteLine(c1.ToString()); //  Pragim.Customer
-            System.Console.WriteLine(Convert.ToString(c1));  //  Pragim.Customer
-            Customer c2 = null;
-            System.Console.WriteLine(Convert.ToString(c2));  // ""
-            System.Console.WriteLine(c2.ToString()); // Exception
+            string name = string.Empty;
+            name += "Tanuj"; // new mmeory space with tanuj
+
+            name += " Nayanam"; // new memory space with tanuj nayanam
+            name += " is"; // new memory space with tanuj nayanam is
+            name += " a";
+            name += " Dumb";
+            name += " Boy";
+            System.Console.WriteLine(name);
+
+            StringBuilder name1 = new StringBuilder();
+            name1.Append("Tanuj");
+            name1.Append(" Nayanam");
+            name1.Append(" is");
+            name1.Append(" a");
+            name1.Append(" Dumb");
+            name1.Append(" Boy");
+            System.Console.WriteLine(name1);
         }
     }
-
-    public class Customer
-    {
-        public string FName { get; set; }
-    }
 }
+
+// output is same bu string is immutable where is stringbuiilder is mutable
+// that means that when ever we assing a new value to streing a separeate memore is create to store the string and old memory is left orphanced. thus we see serious pperfomrance issue. So we should always use string builder because sam mempry space is used. nothing is or[haned
 
