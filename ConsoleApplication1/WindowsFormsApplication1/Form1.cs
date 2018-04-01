150896106f7dc2aaf3433889b05150e1a948c5cf
tnayanam <nayatanuj@gmail.com>
2018-04-01 14:58:37 -0400

    Thread Delegate

--- ConsoleApplication1/WindowsFormsApplication1/Form1.cs
+++ ConsoleApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,15 +16,25 @@ namespace WindowsFormsApplication1
             btnDoTimeConsumingWork.Enabled = false;
             btnPrintNumbers.Enabled = false;
 
-            Thread workerThread = new Thread(DoTimeConsumingWork);
+            Thread workerThread = new Thread(DoTimeConsumingWork); // if we look closely here, we are passing a function delegate. and then in the next line
+            // we are calling the thread instance which in turns calls the delegate which in turn calls the fucnction it points to
+            // if you go the constructor of Thread class you will see the definition as below
+            //public Thread(ThreadStart start); where ThreadStart is of type Delegate
+            //
             workerThread.Start();
             //DoTimeConsumingWork();
+            // Another way of calling worker thread by explicitly mentioing it as delegate
+            //Thread wTh = new Thread(delegate ()
+            //{
+            //    DoTimeConsumingWork();
+            //});
+            //wTh.Start();
 
             btnDoTimeConsumingWork.Enabled = true;
             btnPrintNumbers.Enabled = true;
         }
 
-        public void DoTimeConsumingWork()
+        public static void DoTimeConsumingWork()
         {
             Thread.Sleep(5000);
             MessageBox.Show("Time Consuming Work Ended");
@@ -41,3 +51,4 @@ namespace WindowsFormsApplication1
 }
 
 // Nothing you can do when the time is getting consumed because we have just one threa whih is UI thread. So, we cannot click on print numbers button. the window in unresponsive
+
