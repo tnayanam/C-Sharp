7a5e3d1d76c613884413d89182002e7f36c80bf2
tnayanam <nayatanuj@gmail.com>
2018-04-01 13:52:29 -0400

    Threading Problem

---
+++ ConsoleApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDoTimeConsumingWork = new System.Windows.Forms.Button();
+            this.btnPrintNumbers = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDoTimeConsumingWork
+            // 
+            this.btnDoTimeConsumingWork.Location = new System.Drawing.Point(0, 12);
+            this.btnDoTimeConsumingWork.Name = "btnDoTimeConsumingWork";
+            this.btnDoTimeConsumingWork.Size = new System.Drawing.Size(334, 23);
+            this.btnDoTimeConsumingWork.TabIndex = 0;
+            this.btnDoTimeConsumingWork.Text = "Do Time Consuming Work";
+            this.btnDoTimeConsumingWork.UseVisualStyleBackColor = true;
+            this.btnDoTimeConsumingWork.Click += new System.EventHandler(this.btnDoTimeConsumingWork_Click);
+            // 
+            // btnPrintNumbers
+            // 
+            this.btnPrintNumbers.Location = new System.Drawing.Point(0, 50);
+            this.btnPrintNumbers.Name = "btnPrintNumbers";
+            this.btnPrintNumbers.Size = new System.Drawing.Size(334, 23);
+            this.btnPrintNumbers.TabIndex = 1;
+            this.btnPrintNumbers.Text = "PrintNumbers";
+            this.btnPrintNumbers.UseVisualStyleBackColor = true;
+            this.btnPrintNumbers.Click += new System.EventHandler(this.btnPrintNumbers_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 375);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnPrintNumbers);
+            this.Controls.Add(this.btnDoTimeConsumingWork);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDoTimeConsumingWork;
+        private System.Windows.Forms.Button btnPrintNumbers;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ConsoleApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoTimeConsumingWork_Click(object sender, EventArgs e)
+        {
+            btnDoTimeConsumingWork.Enabled = false;
+            btnPrintNumbers.Enabled = false;
+
+            DoTimeConsumingWork();
+
+            btnDoTimeConsumingWork.Enabled = true;
+            btnPrintNumbers.Enabled = true;
+        }
+
+        public void DoTimeConsumingWork()
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void btnPrintNumbers_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
+
+// Nothing you can do when the time is getting consumed because we have just one threa whih is UI thread. So, we cannot click on print numbers button. the window in unresponsive
