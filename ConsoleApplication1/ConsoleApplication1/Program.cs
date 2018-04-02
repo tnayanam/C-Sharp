beba06e6e9111d13e9a648560e73b65c8234b87b
tnayanam <nayatanuj@gmail.com>
2018-04-01 20:32:10 -0400

    Threading causing issues shared resource

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
+
 class Program
 {
     static int Total = 0;
     public static void Main()
     {
+        Thread th1 = new Thread(AddOneMillion);
         AddOneMillion();
+        Thread th2 = new Thread(AddOneMillion);
         AddOneMillion();
+        Thread th3 = new Thread(AddOneMillion);
         AddOneMillion();
+        th1.Start();
+        th2.Start();
+        th3.Start();
+        th1.Join();
+        th2.Join();
+        th3.Join();
         Console.WriteLine("Total = " + Total);
     }
 
@@ -18,4 +29,5 @@ class Program
         }
     }
 }
-// Single threded we always get 300000 as output
+// Eveytime we are gettig different vslues because Total is a shared resource thus all thread shares its avlue and thus issue occurs.
+// ech thread starts adding to total when total has alredy been incremented by sme counter by other threads. 
