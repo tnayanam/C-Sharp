cc8763e52e7bbb91f12598d50b4ff3a5a5cfdc8a
tnayanam <nayatanuj@gmail.com>
2018-04-01 20:27:10 -0400

    same output with single thread

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,78 +1,21 @@
-﻿using System.Threading;
-
-namespace ThreadStartDelegateExample
+﻿using System;
+class Program
 {
-    // suppose we want to guarantee that main method only ends after threa 1 and thread 2 has been executed. Then we need to do something like below
-    class Program
+    static int Total = 0;
+    public static void Main()
     {
-        public static void Main()
-        {
-            System.Console.WriteLine("Main Thread Started.");
-            Thread T1 = new Thread(ThreadOne);
-            T1.Start();
-            Thread T2 = new Thread(ThreadTwo);
-            T2.Start();
-
-            if (T1.Join(2000)) // it return true if it finds that thread has joined the main thread. which will be No in this case
-            {
-                System.Console.WriteLine("ThreadOne Completed");
-            }
-            else
-            {
-                System.Console.WriteLine("ThreadOne Still Processing");
-            }
-            // 1 sec now this main will just wait for 2 second for the thread `1 to join it. if it does not join main will proceed
-            T2.Join();
-
-            if (T1.IsAlive) // is to check whether T1 is still running or its done
-            {
-                System.Console.WriteLine("ThreadOne Cstill prcessing");
-            }
-            else
-            {
-                System.Console.WriteLine("ThreadOne not compete");
-            }
-            System.Console.WriteLine("ThreadTwo Completed");
-            // so now the main method will not execute unless t1 and t2 finish its execution and merges back to main UI thread
-            System.Console.WriteLine("Main Thread End.");
-
-
-        }
-
-        public static void ThreadOne()
-        {
-            System.Console.WriteLine("ThreadOne Started.");
-            Thread.Sleep(5000);// 5 secs
-            System.Console.WriteLine("ThredaOne is now about to complete");
-        }
-
-        public static void ThreadTwo()
-        {
-            System.Console.WriteLine("ThreadTwo Started.");
-        }
+        AddOneMillion();
+        AddOneMillion();
+        AddOneMillion();
+        Console.WriteLine("Total = " + Total);
     }
 
-
+    public static void AddOneMillion()
+    {
+        for (int i = 1; i <= 1000000; i++)
+        {
+            Total++;
+        }
+    }
 }
-
-// output
-//Main Thread Started.
-//ThreadOne Started.
-//ThreadTwo Started.
-//ThreadOne Still Processing
-//ThreadOne Cstill prcessing
-//ThreadTwo Completed
-//Main Thread End.
-//ThredaOne is now about to complete
-//Press any key to continue . . .
-
-
-
-
-
-
-
-
-
-
-
+// Single threded we always get 300000 as output
