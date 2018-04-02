b91d1861153d13e5b48ea85ee42541ac3ed8db62
tnayanam <nayatanuj@gmail.com>
2018-04-02 01:14:11 -0400

    Non responsive application

---
+++ ConsoleApplication1/AsyncAwait/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            lblCount.Text = "Processing File Please Wait..";
+            lblCount.Text = CountCharacter().ToString(); // as long as the process is completing appliation will be NOT Responsive
+        }
+
+        public static int CountCharacter()
+        {
+            int count = 0;
+            using (StreamReader rdr = new StreamReader("C:\\eula.1028.txt"))
+            {
+                string content = rdr.ReadToEnd();
+                count = content.Length;
+            }
+            Thread.Sleep(3000);
+
+            return count;
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,34 +1,12 @@
 ﻿using System;
-using System.Threading;
 
 class Program
 {
     static int Total = 0;
     public static void Main()
     {
-        Thread th1 = new Thread(AddOneMillion);
-        Thread th2 = new Thread(AddOneMillion);
-        Thread th3 = new Thread(AddOneMillion);
-        th1.Start();
-        th2.Start();
-        th3.Start();
-        th1.Join();
-        th2.Join();
-        th3.Join();
-        Console.WriteLine("Total = " + Total);
-    }
 
-    static object _lock = new object();
-
-    public static void AddOneMillion()
-    {
-        for (int i = 1; i <= 1000000; i++)
-        {
-            lock (_lock)
-            {
-                Total++;
-            }
-        }
+        Console.WriteLine(Environment.ProcessorCount);
     }
 }
 // now it is locked adn we will get 3000000 all the time and it will not change.
