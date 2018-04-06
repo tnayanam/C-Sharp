71d505bfe45107ed694b6e66e6831e6148188a00
tnayanam <nayatanuj@gmail.com>
2018-04-06 03:24:16 -0400

    testing again

--- MainSolution/Sub2/Program.cs
+++ MainSolution/Sub2/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sub2
+﻿namespace Sub2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //sdf
         }
     }
 }
