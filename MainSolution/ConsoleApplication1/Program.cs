b77843050876971a5e1e10064aa745db78181820
tnayanam <nayatanuj@gmail.com>
2018-04-06 03:34:19 -0400

    testing

--- MainSolution/ConsoleApplication1/Program.cs
+++ MainSolution/ConsoleApplication1/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //kl
+            System.Console.WriteLine("Hello WOrld");
         }
     }
 }
