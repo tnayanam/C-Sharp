e24632e07a021f478df6588bc7bc475b0943df63
tnayanam <nayatanuj@gmail.com>
2018-04-07 18:01:45 -0400

    Some more points on Partial Class

--- MainSolution/MainSolution/Program.cs
+++ MainSolution/MainSolution/Program.cs
@@ -16,4 +16,10 @@
  * During run time these partial classes combine into one file.
  * 
  * All parts spread across different files, must use the keyword "partial" keyword.
+ * all the partials for a class must have same access modifiers
+ * if any of the paritals is set to abstract then all the partials for that class gets abstract
+ * different part of partial class cannot put different base class because
+ * c# does not suport multiple class inheritance.
+ * Any member that are declared in the partial is available in other parial too
+ * 
  */
