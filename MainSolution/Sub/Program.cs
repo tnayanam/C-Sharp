808065b299e23b8c8f6ff294e1504effe2512d07
tnayanam <nayatanuj@gmail.com>
2018-04-07 02:49:27 -0400

    Call one exe from another

--- MainSolution/Sub/Program.cs
+++ MainSolution/Sub/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace Sub
 {
@@ -10,6 +6,23 @@ namespace Sub
     {
         static void Main(string[] args)
         {
+            string arg = string.Format("{0} {1}", "Hello", "World");
+            Process.Start(@".\..\..\..\Sub2\bin\Debug\Sub2.exe", arg);
         }
     }
 }
+
+/*
+ * Here we are running two exe together. Basically we want to call
+ * Sub2.exe from this project (Sub.exe). So,  steps are
+ * We need to make sure we give the path properly and also
+ * we need to run Sub.exe and cross the process.start breakpoint.
+ * Now in the same time we need to open other instance of Sub2.exe solution
+ * There we need to add a thread.sleep so that the Sub.exe gices us time to
+ * attach the debugger. MAKE SURE that after adding the sleep we build both Sub and Sub2.exe properly
+ * Now we in other instance put a breakpoint just after the thread.sleep so that
+ * and Now run Sub.exe and once process.strt executes go to othe instance of VS and click onn debug attach to process and
+ * attach the Sub2.exe there. Breakpint will hit now.
+ * Basically the Sub -> calls Sub2.exe ->and if we attach an explicit Sub2.exe in other instace if Sub2.exe it tries to hit breakpoint in it. 
+ * // we need to send data as space separater. 
+ */
--- MainSolution/Sub2/Program.cs
+++ MainSolution/Sub2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //sdf
+            System.Threading.Thread.Sleep(20000);
+            System.Console.WriteLine(args[0]);
+            System.Console.WriteLine(args[1]);
+            System.Console.WriteLine("Sub2 Solution Executed.");
         }
     }
 }
+
+// delimeter is the space. so outputis Hello and then World
+
+
