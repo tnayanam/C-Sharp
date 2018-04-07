b28715852d83940fd2e97b30a554bd8cbc192581
tnayanam <nayatanuj@gmail.com>
2018-04-07 04:19:17 -0400

    One way to handle exception and not allow execution further

--- MainSolution/Sub2/Program.cs
+++ MainSolution/Sub2/Program.cs
@@ -4,12 +4,39 @@
     {
         static void Main(string[] args)
         {
-            System.Threading.Thread.Sleep(20000);
-            System.Console.WriteLine(args[0]);
-            System.Console.WriteLine(args[1]);
-            System.Console.WriteLine("Sub2 Solution Executed.");
+            try
+            {
+                MethodThrowingException(2, 0);
+                // expression 1
+                // expression 2
+            }
+            catch (System.Exception)
+            {
+
+                // now handle the exception again here if you want to
+                // but now expression 1 and expression 2 will not execute during exception
+            }
+
+        }
+
+        public static void MethodThrowingException(int num, int div)
+        {
+            try
+            {
+                int res = num / div;
+                // exp 1 some other expression
+            }
+            catch (System.Exception)
+            {
+                // some exception occured write it to database
+                // exp 1 will not execute
+                throw;
+            }
+
+            // do some operation in db
         }
     }
+
 }
 
 // delimeter is the space. so outputis Hello and then World
