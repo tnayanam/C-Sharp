a5a727df10b749233612a002ca57a681ee40f3c6
tnayanam <nayatanuj@gmail.com>
2018-04-08 13:02:03 -0400

    Convert array to dictionary

--- MainSolution/Sub2/Program.cs
+++ MainSolution/Sub2/Program.cs
@@ -7,62 +7,27 @@ namespace Sub2
     {
         static void Main(string[] args)
         {
+            Customer[] cs = new Customer[5];
             Customer c1 = new Customer { Id = 1, Name = "John", Salary = 4500 };
             Customer c2 = new Customer { Id = 2, Name = "Cena", Salary = 7800 };
             Customer c3 = new Customer { Id = 3, Name = "Check", Salary = 6500 };
             Customer c4 = new Customer { Id = 4, Name = "Smith", Salary = 3500 };
             Customer c5 = new Customer { Id = 5, Name = "Singh", Salary = 500 };
+            cs[0] = c1;
+            cs[1] = c2;
+            cs[2] = c3;
+            cs[3] = c4;
+            cs[4] = c5;
+            // convert array to dict. key is id value is customer itself
 
-            Dictionary<int, Customer> dict = new Dictionary<int, Customer>();
-            dict.Add(c1.Id, c1);
-            dict.Add(c2.Id, c2);
-            dict.Add(c3.Id, c3);
-            dict.Add(c4.Id, c4);
-            dict.Add(c5.Id, c5);
-            // this will throw exception
-            //dict.Add(c1.Id, c5);
-            // checks f key already present., if presernt returns true.
-            if (!dict.ContainsKey(c1.Id))
-            {
-                System.Console.WriteLine("If key is not presernt then only we will add");
-                dict.Add(c1.Id, c5);
-            }
-            // this will throw exception because that key does not exist.
-            //System.Console.WriteLine(dict[34].Name);
 
-            if (dict.ContainsKey(34))
+            Dictionary<int, Customer> dict = cs.ToDictionary(c => c.Id, c => c);
+
+            foreach (var Cust in dict)
             {
-                System.Console.WriteLine("If key is not presernt then only we will add");
-                dict.Add(c1.Id, c5);
+                System.Console.WriteLine(Cust.Value.Name);
             }
 
-            Customer ctemp = new Customer();
-            // if key exists
-            if (dict.TryGetValue(2, out ctemp))
-            {
-                System.Console.WriteLine(ctemp.Name);
-                System.Console.WriteLine(ctemp.Salary);
-            }
-            else
-            {
-                System.Console.WriteLine("key does not exist");
-            }
-            // count
-            System.Console.WriteLine(dict.Count); // 
-            System.Console.WriteLine(dict.Count()); // linq
-            System.Console.WriteLine("");
-            System.Console.WriteLine("Salary greateer then 3000 " + dict.Count(d => d.Value.Salary > 3000)); // linq with condition
-            // delete
-            dict.Remove(2);
-            // delete all items
-            dict.Clear();
-
-            //foreach (var dic in dict)
-            //{
-            //    System.Console.WriteLine("Key: " + dic.Key);
-            //    System.Console.WriteLine("Name: " + dic.Value.Name);
-            //    System.Console.WriteLine("Salary: " + dic.Value.Salary);
-            //}
         }
     }
 
