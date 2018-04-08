4a6d6a80c9e1a7d6ec748484f09440f397828d67
tnayanam <nayatanuj@gmail.com>
2018-04-08 01:12:22 -0400

    optional parameter, specifying a particular parameter

--- MainSolution/Sub2/Program.cs
+++ MainSolution/Sub2/Program.cs
@@ -1,23 +1,17 @@
-﻿using System.Linq;
-
-namespace Sub2
+﻿namespace Sub2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Add(5, 10);
+            Add(5, num3: 2); // I only want to pass value fo num1 and num 3
         }
 
-        public static void Add(int num1, int num2, int[] nums = null)
+        public static void Add(int num1, int num2 = 4, int num3 = 8)
         {
-            int sum = 0;
-            sum = num1 + num2;
-            if (nums != null)
-            {
-                sum += nums.Sum();
-            }
-            System.Console.WriteLine(sum);
+            System.Console.WriteLine("num1: " + num1);
+            System.Console.WriteLine("num2: " + num2);
+            System.Console.WriteLine("num3: " + num3);
         }
     }
 
