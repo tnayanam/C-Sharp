1396b96c07a0c64f49ba81256e80f581c94f09d1
Tanuj Nayanam <tnayanam@sessionm.lan>
2018-08-21 13:19:04 -0400

    Working with resx file

---
+++ Resx/Resx/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Reflection;
+using System.Resources;
+using System.Globalization;
+
+namespace Resx
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResourceManager rm = new ResourceManager("Resx.Resource1",
+Assembly.GetExecutingAssembly());
+            var t = Assembly.GetExecutingAssembly(); // this gives the resources that are getting loaded
+            String strWebsite = rm.GetString("Website", CultureInfo.CurrentCulture);
+            String strName = rm.GetString("Name");
+            form1.InnerText = "Website: " + strWebsite + "--Name: " + strName;
+        }
+    }
+}
