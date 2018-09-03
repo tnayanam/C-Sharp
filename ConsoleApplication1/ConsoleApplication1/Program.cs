80f169cd3a08e2eb7084418bf94003c08a1b3898
tnayanam <nayatanuj@gmail.com>
2018-09-02 23:22:55 -0400

    Async Await example

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Three things to note in the signature:  
+//  - The method has an async modifier.   
+//  - The return type is Task or Task<T>. (See "Return Types" section.)  
+//    Here, it is Task<int> because the return statement returns an integer.  
+//  - The method name ends in "Async." 
+
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
@@ -10,6 +13,23 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            var task = AcessTheWebAsync();
+            task.Wait(); // this is very important, otherwise program execution will end and await thread will terminate.
+            System.Console.WriteLine(task.Result);
+        }
+
+        public static async Task<int> AcessTheWebAsync()
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com"); // here a different thread is assigned
+            DoIndependentWork(); // main thread continues here
+            string urlContents = await getStringTask; // Now here the main thread suspends and goes back to the caller. 
+            return urlContents.Length;
+        }
+
+        private static void DoIndependentWork()
+        {
+            // do all the work here for which you dont need the output of GetStringAsync
         }
     }
 }
