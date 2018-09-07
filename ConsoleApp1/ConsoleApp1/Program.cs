fbb6c3c6b3a3cb4033d08bcb7a168e8811f8f30f
Tanuj Nayanam <tnayanam@sessionm.com>
2018-09-06 21:16:51 -0400

    How to create a Task and run it

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int x = 0; x < 50; x++)
+                {
+                    Console.Write("Hi ");
+                }
+            });
+            t.Wait();
+        }
+    }
+}
