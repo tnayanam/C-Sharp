e7d94f880db338cd299f9a538313cd98628092d0
Tanuj Nayanam <tnayanam@sessionm.com>
2018-09-20 11:00:39 -0400

    YIELD

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+    // so everytime yield is called GetNumbers() get hit and yield returns to the caller one by one and result is processed.
     class Program
     {
         static void Main(string[] args)
         {
-            Task t = Task.Run(() =>
+            foreach (var num in GetNumbers())
             {
-                for (int x = 0; x < 50; x++)
-                {
-                    Console.Write("Hi ");
-                }
-            });
-            t.Wait();
+                Console.WriteLine(num);
+            }
         }
+        public static IEnumerable<int> GetNumbers() { yield return 1; yield return 2; yield return 3; }
     }
 }
