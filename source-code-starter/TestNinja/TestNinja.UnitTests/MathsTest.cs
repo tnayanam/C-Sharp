36031411089adaac3fe3323e0186c999078eefde
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-13 16:46:07 -0500

    Unit test where testing function has parameters

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathsTest
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
