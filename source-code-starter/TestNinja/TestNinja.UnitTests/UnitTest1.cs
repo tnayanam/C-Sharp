c3e395dfae328ac93aca1d9c6e3d43fe4c2df73d
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-13 14:04:22 -0500

    first unit test

--- source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ReservationTests // Notice this convention. class tat we are trying to test followed by "tests"
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() // convention methodname_scenariobeingtested_expectedoutcome
         {
+            // Arrange
+            var reservation = new Reservation();
+            // Act
+           var result =  reservation.CanBeCancelledBy(new User {IsAdmin = true});
+            //Assert
+            Assert.IsTrue(result);
         }
     }
 }
