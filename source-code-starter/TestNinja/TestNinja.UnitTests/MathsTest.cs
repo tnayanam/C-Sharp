05eb5e6e0b809c4ffa98b772d312ef2938fe6a20
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-14 10:56:35 -0500

    SetUp  method as a constructor

--- source-code-starter/TestNinja/TestNinja.UnitTests/MathsTest.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathsTest.cs
@@ -11,35 +11,39 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathsTest
     {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnSumOfArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentGreater_ReturnFirstArguments()
         {
-            var math = new Math();
-            var result = math.Max(2,1);
+            var result = _math.Max(2,1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumentGreater_ReturnSecondArguments()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsEqual_ReturnSameArguments()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
