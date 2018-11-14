325bb5aa15e066a1a82484a507be5b22f0984d5d
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-14 14:22:34 -0500

    test void methods

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+   public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
