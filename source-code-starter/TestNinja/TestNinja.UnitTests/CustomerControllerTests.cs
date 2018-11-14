61654e86c81254c9452180f02d74d768727324d5
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-14 13:58:19 -0500

    InstanceOf and TypeOf testing

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            // One Way
+            var result = controller.GetCustomer(0);
+             Assert.That(result, Is.TypeOf<NotFound>()); // this means result should be of exact "Not Found" type
+
+            // ANother Way
+            Assert.That(result, Is.InstanceOf<NotFound>()); // this means result should be of exact "Not Found" type or any of tis derivatives
+
+        }
+    }
+}
