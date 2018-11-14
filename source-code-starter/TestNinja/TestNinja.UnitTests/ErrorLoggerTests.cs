d3f9130d81c23b5665d478c36bd20a50401ebfe9
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-14 14:32:30 -0500

    Test method that raises an Exceptions

--- source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -19,5 +19,16 @@ namespace TestNinja.UnitTests
             logger.Log("a");
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error),Throws.ArgumentNullException);
+        }
     }
 }
