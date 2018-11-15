2affdca29f621d45e844d6e43a507f8b93145e40
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-15 14:24:17 -0500

    Method 1 to inject dependencies

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_RetirnError()
+        {
+            var svc = new VideoService();
+            var result = svc.ReadVideoTitle(new FakeFileReader()); // this is how we are injecting the dependencies.
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
--- source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
+++ source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
@@ -9,10 +9,12 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public string ReadVideoTitle()
+        public string ReadVideoTitle(IFileReader fileReader)
         {
-           // var str = File.ReadAllText("video.txt"); // this is the place where we are dealing with an external class/source which is "video.txt"
-            var str = new FileReader().Read("video.txt"); // this is still tightly coupled with the FileReader class because we are "newing" that class
+            // var str = File.ReadAllText("video.txt"); // this is the place where we are dealing with an external class/source which is "video.txt"
+            var str = fileReader.Read("video.txt"); // this is still tightly coupled with the FileReader class because we are "newing" that class
+            // str will be empty string when called usinf FakeFileReader becuse Read will return "" and thus videeo will be null as it cannot be serlilized.
+                // so this was method1 to inject dependency
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
@@ -22,14 +24,14 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
-            
+
             using (var context = new VideoContext())
             {
-                var videos = 
+                var videos =
                     (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
+                     where !video.IsProcessed
+                     select video).ToList();
+
                 foreach (var v in videos)
                     videoIds.Add(v.Id);
 
