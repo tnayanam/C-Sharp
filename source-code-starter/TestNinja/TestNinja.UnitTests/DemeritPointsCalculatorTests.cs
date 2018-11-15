5f64eee5a71caadca055ae4148d3ce31cc08862a
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-15 12:43:47 -0500

    some more testing

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SppedNotGood_ArgsOutOfRange(int speed)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            Assert.That(()=> demeritPointsCalculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(4,0)]
+        [TestCase(65,0)]
+        [TestCase(66,0)]
+        [TestCase(70,1)]
+        [TestCase(75,2)]
+        public void CalculateDemeritPoints_WehnCalled_Zero(int speed, int output)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            Assert.That(() => demeritPointsCalculator.CalculateDemeritPoints(speed), Is.EqualTo(output));
+        }
+    }
+}
---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_ArgsIsNull_ArgsNullException()
+        {
+            var stack = new Stack<string>();
+            Assert.That(()=>stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddtheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EMptyStack_RetirnZero()
+        {
+            var stack = new Stack<string>();
+            Assert.That(stack.Count,Is.EqualTo(0));
+        }
+
+
+    }
+}
