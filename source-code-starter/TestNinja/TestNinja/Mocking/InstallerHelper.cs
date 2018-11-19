6a87672be4d04eb5515a7e7d56a8f488b8aaad23
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-19 14:15:09 -0500

    Interface is separated for test stubs

--- source-code-starter/TestNinja/TestNinja/Mocking/InstallerHelper.cs
+++ source-code-starter/TestNinja/TestNinja/Mocking/InstallerHelper.cs
@@ -1,27 +1,32 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
         private string _setupDestinationFile;
+        private IFileDownloader _fileDownloader;
+
+        public InstallerHelper(IFileDownloader _fileDownloader)
+        {
+            _fileDownloader = _fileDownloader ?? new FileDownloader();
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
+            _fileDownloader.DownloadFile(string.Format("http://example.com/{0}/{1}",
+                    customerName,
+                    installerName)
+                 , _setupDestinationFile);
 
                 return true;
             }
             catch (WebException)
             {
-                return false; 
+                return false;
             }
         }
     }
