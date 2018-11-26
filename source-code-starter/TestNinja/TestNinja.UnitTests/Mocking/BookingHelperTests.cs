f8009ed566cfd760b6fbfe6320ad1ab4bebaa94f
Tanuj Nayanam <tnayanam@sessionm.com>
2018-11-26 14:06:57 -0500

    Extracted interface

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperTests_OverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingMeeting_ReturnEmptyString()
+        {
+
+        }
+    }
+}
---
+++ source-code-starter/TestNinja/TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null) // we are returning IQueryable here because all the methods that is referencing it are writing 
+        // LINQ query against it.
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b =>  b.Status != "Cancelled");
+            if (excludedBookingId.HasValue)
+                bookings = bookings.Where(b => b.Id != excludedBookingId.Value);
+            return bookings;
+        }
+    }
+}
