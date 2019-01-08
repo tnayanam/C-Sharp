8ddaacc12838d193aba1f72a26136c84f90c2f9d
Tanuj Nayanam <tnayanam@sessionm.com>
2019-01-08 15:25:00 -0500

    Stop Watch to measure functionn execution time

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -1,30 +1,53 @@
-﻿namespace ConsoleApp8
+﻿using System;
+
+namespace ConsoleApp8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Publisher dogPublisher = new Publisher(); // this is sending request about dog stuff
-            Publisher catPublisher = new Publisher(); // this is sending request about cat stuff
-            PubSubServer server = new PubSubServer();
-            Subscriber animalLover = new Subscriber(); // wants to subscribe to all messages from publisher
-            Subscriber catLover = new Subscriber(); // wants to subscribe to cat related message from publisher
-            // animal lover wants to know about both publisher
-            // cat lover wants to know only sbout catPublisher
-            Message dogMessage = new Message { payload = "This is dog paylod", topic = "Dogs" };
-            Message catMessage = new Message { payload = "This is cat paylod", topic = "Cats" };
-            dogPublisher.Send(server, dogMessage); // all these messages are getting enqueued in the server
-            catPublisher.Send(server, catMessage); // all these messages are getting enqueued in the server
-            server.subscribers[0] = animalLover;
-            server.subscribers[1] = catLover;
-            // Now here we will tell which publisher is interested in which type of subscriber
-            animalLover.Listen("Dogs", 0);
-            animalLover.Listen("Cats", 1);
-            // cat lover is only interested in cats topic
-            catLover.Listen("Cats", 0);
-            server.Forward();
-            animalLover.Print();
-            catLover.Print();
+
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            watch.Reset();
+            //watch.Start();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            watch.Stop(); // STOPS THE WATCH
+
+
+            //Publisher dogPublisher = new Publisher(); // this is sending request about dog stuff
+            //Publisher catPublisher = new Publisher(); // this is sending request about cat stuff
+            //PubSubServer server = new PubSubServer();
+            //Subscriber animalLover = new Subscriber(); // wants to subscribe to all messages from publisher
+            //Subscriber catLover = new Subscriber(); // wants to subscribe to cat related message from publisher
+            //// animal lover wants to know about both publisher
+            //// cat lover wants to know only sbout catPublisher
+            //Message dogMessage = new Message { payload = "This is dog paylod", topic = "Dogs" };
+            //Message catMessage = new Message { payload = "This is cat paylod", topic = "Cats" };
+            //dogPublisher.Send(server, dogMessage); // all these messages are getting enqueued in the server
+            //catPublisher.Send(server, catMessage); // all these messages are getting enqueued in the server
+            //server.subscribers[0] = animalLover;
+            //server.subscribers[1] = catLover;
+            //// Now here we will tell which publisher is interested in which type of subscriber
+            //animalLover.Listen("Dogs", 0);
+            //animalLover.Listen("Cats", 1);
+            //// cat lover is only interested in cats topic
+            //catLover.Listen("Cats", 0);
+            //server.Forward();
+            //animalLover.Print();
+            //catLover.Print();
         }
     }
 }
